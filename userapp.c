84fb50ff2cd25736e490f78b9f7e979726ac7710
Cass-1 <cass.dahle@gmail.com>
2023-11-19 21:56:03 -0800

    made a proper test case

--- userapp.c
+++ userapp.c
@@ -8,11 +8,42 @@
 // Register process with kernel module
 void register_process(unsigned int pid)
 {
-    
      // Insert your code here ...
     FILE* file = fopen("/proc/kmlab/status", "w");
     fprintf(file, "%u", pid);
     fclose(file);
+}
+
+// print out proc file
+int print_procfs(){
+    FILE* file = fopen("/proc/kmlab/status", "r");
+    char c;
+    if (file == NULL) 
+    { 
+        printf("Cannot open file \n"); 
+        return 1;
+    } 
+  
+    // Read contents from file 
+    c = fgetc(file); 
+    while (c != EOF) 
+    { 
+        printf ("%c", c); 
+        c = fgetc(file); 
+    } 
+    fclose(file);
+    return 0;
+}
+
+// a slow fibbonachi function
+int slow_fibbonacci(int n) {
+   if(n == 0){
+      return 0;
+   } else if(n == 1) {
+      return 1;
+   } else {
+      return (slow_fibbonacci(n-1) + slow_fibbonacci(n-2));
+   }
 }
 
 int main(int argc, char* argv[])
@@ -25,12 +56,15 @@ int main(int argc, char* argv[])
     }
 
     register_process(getpid());
-
+    print_procfs();
+    
     // Terminate user application if the time is expired
     while (1) {
         if ((int)(time(NULL) - start_time) > __expire) {
             break;
         }
+        slow_fibbonacci(41);
+        print_procfs();
     }
 
 	return 0;
